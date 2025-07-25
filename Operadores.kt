08b45297f5f897cd549724297aba3d8e9388993d
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-07-25 14:12:05 -0600

    Create Operadores.kt
    Operadores Aritmeticos en Kotlin

---
+++ Operadores.kt
@@ -0,0 +1,18 @@
+
+//Operadores en kotlin
+fun main() {
+    val num1 = 14
+    val num2 = 17
+    println("Ingresa un numero: ${num1}")
+    println("Ingresa otro numero: ${num2}")
+    val suma = num1 + num2
+    val resta = num1 - num2
+    val multiplicacion = num1 * num2
+    val division = num1 / num2
+    val residuo = num1 % num2 
+    println("La suma de ${num1} + ${num2} = ${suma}")
+    println("La resta de ${num1} - ${num2} = ${resta}")
+    println("La multiplicacion de ${num1} x ${num2} = ${multiplicacion}")
+    println("La division de ${num1} / ${num2} = ${division}")
+    println("La residuo de ${num1} % ${num2} = ${residuo}")
+}
