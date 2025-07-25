5c88261c4d83a01630c504315550145cd9760b10
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-07-25 16:28:04 -0600

    Agrega y explica dos tipos d variables en Kotlin

---
+++ Variables.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val nombre = "Gabriel" //Val es para valores constantes, no cambian
+    var edad = 20; //Var es para valores variables, iran cambiando
+    println("Mi nombre es: ${nombre}")
+    println("En este momento tengo ${edad} años")
+}
