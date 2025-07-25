01dc69cbf8a164b47fa4e35781312cbf0a81daec
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-07-25 17:53:26 -0600

    Se agregaron mas variables

--- Variables.kt
+++ Variables.kt
@@ -1,6 +1,10 @@
 fun main(){
     val nombre = "Gabriel" //Val es para valores constantes, no cambian
-    var edad = 20; //Var es para valores variables, iran cambiando
+    var edad = 20 //Var es para valores variables, iran cambiando
+    val altura = 1.70
+    val mayorEdad = (edad >= 18)
     println("Mi nombre es: ${nombre}")
     println("En este momento tengo ${edad} años")
+    printn("Mido ${altura}")
+    prinntl("Soy mayor de edad: ${mayorEdad}")
 }
