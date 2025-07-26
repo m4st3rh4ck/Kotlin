eadbec44bb989c2000952805bbea6a079960c4a2
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-07-25 18:05:38 -0600

    Se creo una nueva rama llamada practica y se agrego genero como nueva variable

--- Variables.kt
+++ Variables.kt
@@ -2,10 +2,13 @@ fun main(){
     val nombre = "Gabriel" //Val es para valores constantes, no cambian
     var edad = 20 //Var es para valores variables, iran cambiando
     val altura = 1.70
+    val genero = "Masculino"
     val mayorEdad = (edad >= 18)
     println("Mi nombre es: ${nombre}")
     println("En este momento tengo ${edad} años")
     printn("Mido ${altura}")
     prinntl("Soy mayor de edad: ${mayorEdad}")
     //Comentario de prueba
+    println("Mi genero es: ${genero}")
+    //Otro comentario de prueba para Git
 }
